2d2ec168671714346c970f7fca33ea82c1efe6fb
Tux <34108937+Tux9001@users.noreply.github.com>
2020-05-18 17:43:46 -0400

    now it works when you run it

--- dealphabetize.py
+++ dealphabetize.py
@@ -3,5 +3,5 @@ from string import ascii_lowercase
 def dealphabetize(text):
     return ' '.join([ascii_lowercase.replace(char, '') for char in text])
 
-if __name__ == "main":
+if __name__ == "__main__":
     print(dealphabetize(input()))
