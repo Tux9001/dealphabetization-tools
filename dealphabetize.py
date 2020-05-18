910c244c78442c988bd88560a9a2bca130aeab5c
Tux <34108937+Tux9001@users.noreply.github.com>
2020-05-18 17:37:58 -0400

    fixed everything lol

--- dealphabetize.py
+++ dealphabetize.py
@@ -1,7 +1,7 @@
 from string import ascii_lowercase
 
-def dealphabetize():
+def dealphabetize(text):
     return ' '.join([ascii_lowercase.replace(char, '') for char in text])
 
 if __name__ == "main":
-	print dealphabetize(input())
+    print(dealphabetize(input()))
